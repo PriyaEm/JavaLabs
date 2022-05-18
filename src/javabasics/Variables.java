971ffa680aa97cbd5d21d188a337dee3c979b29c
priya <priya.medalist@gmail.com>
2022-05-18 09:46:35 +0530

    added variable class

---
+++ src/javabasics/Variables.java
@@ -0,0 +1,80 @@
+package javabasics;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		/*
+		 * Byte
+		 * Min: -128
+		 * Max: 127
+		 * Default: 0
+		 */
+		
+		byte byteVaribale = 100;
+		System.out.println("Byte value:" + byteVaribale);
+		
+		/*
+		 * Short
+		 * Min: -32,768
+		 * Max: 32,767
+		 * Default: 0
+		 */
+		short shortVariable = 15000;
+		System.out.println("Short Value:" + shortVariable);
+
+		/*
+		 * int
+		 * Min: -2,147,483,648
+		 * Max: 2,147,483,647
+		 * Default: 0
+		 */
+		int intVariable = 10;
+		System.out.println("Int Value:" + intVariable);
+		
+		/*
+		 * long
+		 * Min: -2^63
+		 * Max: 2^63 -1
+		 * Default: 0
+		 */
+		long longVariable = 1234567899L;
+		System.out.println("Long Value:"+ longVariable);
+		
+		/*
+		 * float
+		 * Default: 0.0f
+		 */
+		
+		float floatVariable = 10.1f;
+		System.out.println("Float Value:" + floatVariable);
+		
+		/*
+		  * double
+		  * Default: 0.0d
+		  */
+		double doubleVariable = 10.25;
+		System.out.println("Double Value:" + doubleVariable);
+		
+		/*
+		  * boolean
+		  * false and true
+		  * Default: false
+		  */
+		
+		boolean booleanVariable = true;
+		System.out.println("Boolean Value:" + booleanVariable);
+		
+		 /*
+		  * char
+		  * Min: 0
+		  * Max: 65,535
+		  */
+		char charVariable = 'a';
+		System.out.println("Char Value:" + charVariable);
+		
+		
+		
+		
+	}
+
+}
